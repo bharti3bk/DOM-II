aafbf87a4a6dbe367f200b6db4effd5bde47c161
bharti3bk <bharti3bk@gmail.com>
2019-06-18 17:56:24 -0400

    Added scroll event

--- js/index.js
+++ js/index.js
@@ -1,28 +1,58 @@
 
 const nav = document.querySelectorAll('nav a')
-
 nav[0].addEventListener('mouseover', function (event) {
     event.target.style.color = 'Red';
 });  
 
-const paragraph = document.querySelector('.intro p') 
-
-paragraph.addEventListener('dblclick' , function (event) {
+ document.querySelector('.intro p').addEventListener('dblclick' , function (event) {
     event.target.style.visibility = 'hidden';
 })  
 
-const fourthHeading = document.querySelector('.destination h4')
-fourthHeading.addEventListener('select' ,function (event) {
-   event.target.style.color = 'Red';
+ document.querySelector('h2').addEventListener('click' , function (event) {
+     window.alert("Hey There");
+ })     
+
+ document.querySelector('.text-content p').addEventListener('mouseleave' , function (event) {
+    event.target.style.color = 'Green';
+  
+ })    
+ window.addEventListener('scroll', function(event) {
+     this.window.alert("you are scrolling");
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ document.querySelector('.logo-heading').addEventListener('pageshow' , function(event) {
+     event.target.style.color = "lightpink";
+ })
+
+
+ document.querySelector('.destination h4').addEventListener('select' ,function (event) {
+  window.alert("You have selected me ");
+
 })  
 
-const resizing = window.addEventListener('resize', function (event) {
+window.addEventListener('resize', function (event) {
  event.target.style.fontSize = '100px;';
-})  
+})     
 
-const maincontainer = document.querySelector('.container').addEventListener('keydown' , function(event) {
-    alert("Hey There !");
-})
+
+
+
+
+
 
 
 
