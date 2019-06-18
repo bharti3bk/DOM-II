1ba5191ea4304961b0f81ebbd78340de71235b73
bharti3bk <bharti3bk@gmail.com>
2019-06-18 17:04:31 -0400

    Updated file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-// Your code goes here
+
+const nav = document.querySelectorAll('nav a')
+
+nav[0].addEventListener('mouseover', function (event) {
+    event.target.style.color = 'Red';
+});  
+
+const paragraph = document.querySelector('.intro p') 
+
+paragraph.addEventListener('dblclick' , function (event) {
+    event.target.style.visibility = 'hidden';
+})  
+
+const fourthHeading = document.querySelector('.destination h4')
+fourthHeading.addEventListener('select' ,function (event) {
+   event.target.style.color = 'Red';
+})  
+
+const resizing = window.addEventListener('resize', function (event) {
+ event.target.style.fontSize = '100px;';
+})  
+
+const maincontainer = document.querySelector('.container').addEventListener('keydown' , function(event) {
+    alert("Hey There !");
+})
+
+
+
+
+
+
