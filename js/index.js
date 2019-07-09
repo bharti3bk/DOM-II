8fe774fc720396ea1c745d6fe7a1b186bde9197e
bharti3bk <bharti3bk@gmail.com>
2019-07-09 13:35:02 -0700

    Added EventListner on nav section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
+const nav = document.querySelectorAll('nav a')
+nav[0].addEventListener('mouseover', function (event) {
+    event.target.style.color = 'Maroon'; 
+});  
 
+ document.querySelector('.intro p').addEventListener('dblclick' , function (event) {
+    event.target.style.visibility = 'hidden';
+}) 
 
 
 
